9c891d3f63a111f671b530ea727037a3ba8d84f5
sabricetra <sabrina.cetra@gmail.com>
2022-04-13 20:56:24 -0300

    corregi el parentesis

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath) );
 
- app.listen(process.env.PORT || 3001, () => console.log("servidor corriendo en el puerto 3000")
+ app.listen(process.env.PORT || 3001, () => console.log("servidor corriendo en el puerto 3000"))
 
  app.get("/", (req, res)=> {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
